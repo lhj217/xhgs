e5ddf2250f52222271ed982a59cd267ca60c8e83
Fordring <playleud@163.com>
2016-11-06 19:50:46 +0800

    add app interface

---
+++ src/main/java/com/dongfu/controller/app/NRS0001Controller.java
@@ -0,0 +1,40 @@
+package com.dongfu.controller.app;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * APP首页交易
+ * 
+ * @ClassName NRS0001Controller.java
+ * @Description TODO
+ * @Author Fordring
+ * @Date 2016年11月6日
+ * @Version V1.0
+ */
+@Controller
+@RequestMapping("/app")
+public class NRS0001Controller {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	@ResponseBody
+	@RequestMapping(value = "/NRS0001", method = { RequestMethod.GET })
+	public Map<String, Object> XFS0001() {
+		Map<String, Object> result = new HashMap<String, Object>(5);
+		// 首页小说
+		List<Map<String, Object>> homeBooks = sqlSession.selectList("t_book_info.findByShowHomePage");
+		result.put("books", homeBooks);
+		return result;
+	}
+
+}
